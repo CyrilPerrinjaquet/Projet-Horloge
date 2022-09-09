e360921d2ac4e1b2c1b7f28fedacc78d45471ace
CyrilPerrinjaquet <111278748+CyrilPerrinjaquet@users.noreply.github.com>
2022-09-09 14:50:09 +0200

    replaced the img and added the refresh and parameters of the needle

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function refreshClock() {}
+let date, hr, min, sec
+function refreshClock() {
+    date = new Date()
+    hr = date.getHours()
+    min = date.getMinutes()
+    sec = date.getSeconds()
 
-window.setInterval(refreshClock, 1000);
+    document.querySelector(".hr").style.transform = 
+    `translate(-50%, -100%) rotate(${hr * 360 / 12}deg)`
+    document.querySelector(".min").style.transform = 
+    `translate(-50%, -100%) rotate(${min * 360 / 60 }deg)`
+    document.querySelector(".sec").style.transform = 
+    `translate(-50%, -100%) rotate(${sec * 360 / 60}deg)`
+}
+refreshClock()
+window.setInterval(refreshClock, 1000)
